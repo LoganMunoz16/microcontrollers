2f95ed80c302699db8d4501e6806add527e1978b
Logan Munoz <munozl1@spu.edu>
2023-05-11 21:20:03 -0700

    Adding Light Sensor Calibration

--- main.py
+++ main.py
@@ -3,25 +3,37 @@ import array
 import utime
 import rp2
 
-LIGHT_READING_UPPER_LIMIT = 6000
-LIGHT_READING_LOWER_LIMIT = 100
-
 # Light Sensor (LDR) Class
 class LightSensor():
     def __init__(self, pin):
         self.adc_pin = ADC(pin)
+        self.calibrate_light()
         
     def get_lux(self):
         return self.adc_pin.read_u16()
+    
+    def calibrate_light(self):
+        print("Light calibration in progress...")
+        sum = 0
+        for i in range(0,10):
+            sum = sum + self.adc_pin.read_u16()
+            utime.sleep(0.5)
+            
+        self.LIGHT_READING_UPPER_LIMIT = (sum / 10.0) + 2000
+        self.LIGHT_READING_LOWER_LIMIT = (sum / 10.0) - 2000
+        print("UPPER:", self.LIGHT_READING_UPPER_LIMIT)
+        print("LOWER:", self.LIGHT_READING_LOWER_LIMIT)
+        
+    
     # We will have to fine tune this function
     def light_is_on(self):
         # get light level reading
         light_level = self.adc_pin.read_u16()
         # compare reading to lower and upper limit
-        if (light_level < LIGHT_READING_LOWER_LIMIT or light_level > LIGHT_READING_UPPER_LIMIT):
-            return False
-        else:
+        if (light_level < self.LIGHT_READING_LOWER_LIMIT or light_level > self.LIGHT_READING_UPPER_LIMIT):
             return True
+        else:
+            return False
 
 # END_CLASS
 
@@ -74,25 +86,26 @@ led = RGBLED()
 trigger = Pin(16, Pin.OUT)
 echo = Pin(17, Pin.IN)
 buzzer = PWM(Pin(20))
+buzzer.duty_u16(0)
 light_sensor = LightSensor(27)
 
-def alarm():
+def toggle_alarm():
     buzzer.duty_u16(32767)
-    #red.value(1)
-    #blue.value(0)
     buzzer.freq(1000)
     led.pixels_fill((255,0,0))
     led.pixels_show()
-    utime.sleep(1)
-    #blue.value(1)
-    #red.value(0)
+    utime.sleep(0.5)
     buzzer.freq(500)
     led.pixels_fill((0,0,255))
     led.pixels_show()
-    utime.sleep(1)
+    utime.sleep(0.5)
     buzzer.duty_u16(0)
     led.pixels_fill((0,0,0))
     led.pixels_show()
+
+def alarm():
+    for i in range(0,10):
+        toggle_alarm()
 
 def ultra():
     trigger.low()
@@ -116,5 +129,8 @@ def ultra():
         led.pixels_show()
 
 while True:
+    buzzer.duty_u16(0)
+    utime.sleep(1)
     ultra()
     utime.sleep(0.5)
+
