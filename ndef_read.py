f77f9d9d9a5772c8c79e386c3a7a33a82e7a999d
Tu <vot4@spu.edu>
2023-05-18 14:34:15 -0700

    Optimized RFID

--- ndef_read.py
+++ ndef_read.py
@@ -1,8 +1,8 @@
+from machine import Pin
 from mfrc522 import MFRC522
 import utime
-from machine import Pin
 
-reader = MFRC522(spi_id=0,sck=2,miso=4,mosi=3,cs=1,rst=0)
+reader = MFRC522(spi_id=0, sck=2, miso=4, mosi=3, cs=1, rst=0)
 led = Pin(25, Pin.OUT)
 
 print("")
@@ -10,6 +10,8 @@ print("Place card into reader")
 print("")
 
 PreviousCard = [0]
+registered_uids = [[0x03, 0x5A, 0x0C, 0x0E]]
+
 
 try:
     while True:
@@ -21,19 +23,20 @@ try:
                 continue
 
             if stat == reader.OK:
-                led.value(1)
-                print("Card detected {}  uid={}".format(hex(int.from_bytes(bytes(uid),"little",False)).upper(),reader.tohexstring(uid)))
-                firstSectorKey = [0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5]
-                nextSectorKey = [0xD3, 0xF7, 0xD3, 0xF7, 0xD3, 0xF7]
+                print("Card detected {}  uid={}".format(hex(int.from_bytes(bytes(uid), "little", False)).upper(), reader.tohexstring(uid)))
 
-                if reader.MFRC522_DumpClassic1K(uid, Start=0, End=4, keyA=firstSectorKey)== reader.OK:
-                    reader.MFRC522_DumpClassic1K(uid, Start=4, End=64, keyA=nextSectorKey)
-                print("Done")
+                if uid in registered_uids:
+                    print("Registered card!")
+                    led.value(1)
+                    utime.sleep_ms(1000)
+                    led.value(0)
+                else:
+                    print("Unregistered card!!")
+
                 PreviousCard = uid
         else:
-            PreviousCard=[0]
-            led.value(0)
-        utime.sleep_ms(50)                
+            PreviousCard = [0]
+        utime.sleep_ms(50)
 
 except KeyboardInterrupt:
     print("Bye")
